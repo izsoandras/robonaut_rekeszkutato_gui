e53fd288d5d626e3503bc5d2df94afcaeb0db34f
Izsó András <izso.andras@simonyi.bme.hu>
2021-11-21 20:43:03 +0100

    Working spectogram like diagram implementation for line sensor data

--- my_gui/plotting/SpectogramBinary.py
+++ my_gui/plotting/SpectogramBinary.py
@@ -6,5 +6,5 @@ class SpectogramBinary(Spectogram):
         Spectogram.__init__(self, subplot, dholders, recipe, sample_num, num_in_sample)
 
     def transform_data(self, data):
-        new_data = [[int(x) for x in bin(d)[2:]] for d in data]
+        new_data = [[int(x) for x in bin(d)[2:].zfill(self.width)][::-1] for d in data]
         return super().transform_data(new_data)
