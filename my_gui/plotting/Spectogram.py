7029e565efe194583890669712da51b6107509de
Izsó András <izso.andras@simonyi.bme.hu>
2021-11-22 13:15:33 +0100

    Added flip options

--- my_gui/plotting/Spectogram.py
+++ my_gui/plotting/Spectogram.py
@@ -9,6 +9,17 @@ class Spectogram(AbstractDiagram):
     def __init__(self, subplot, dholders: dict, recipe: dict, sample_num: int = 50, num_in_sample: int = 32):
         self.quad = None
         self.idx = 0
+
+        if('flipud' in recipe.keys()):
+            self.flipud = recipe['flipud']
+        else:
+            self.flipud = False
+
+        if('fliplr' in recipe.keys()):
+            self.fliplr = recipe['fliplr']
+        else:
+            self.fliplr = False
+
         self.width = num_in_sample
         self.height = sample_num
         self.data = np.zeros((self.width,self.height))
@@ -27,7 +38,14 @@ class Spectogram(AbstractDiagram):
 
     def transform_data(self, data):
         mtx_data = np.array(data)
-        return np.flipud(mtx_data)
+
+        if self.flipud:
+            mtx_data = np.flipud(mtx_data)
+
+        if self.fliplr:
+            mtx_data = np.fliplr(self.fliplr)
+
+        return mtx_data
 
     def update_data(self):
         data_key = list(self.axs.keys())[0]
@@ -39,4 +57,5 @@ class Spectogram(AbstractDiagram):
 
         for key in self.dataholders.keys():
             self.data = self.transform_data(self.dataholders[key].getData(data_key))
+
             self.quad.set_array(self.data.ravel())
