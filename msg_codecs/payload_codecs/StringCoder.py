38f9722864d94e51c9692810183d85a0bccdf428
Izsó András <izso.andras@simonyi.bme.hu>
2022-01-12 16:17:14 +0100

    StringCoder fix

--- msg_codecs/payload_codecs/StringCoder.py
+++ msg_codecs/payload_codecs/StringCoder.py
@@ -5,10 +5,14 @@ class StringCoder(PayloadCoder):
     def __init__(self, name: str, type: int, fields, format='string'):
         if format != 'string':
             raise ValueError("Required format is string!")
+
+        if len(fields) != 1:
+            raise ValueError("String message must have exactly 1 field!")
+
         PayloadCoder.__init__(self, name, type, 's', fields)
 
     def encode(self, data: dict):
         return data[self.field_names[0]].encode('utf-8')
 
     def decode(self, payload):
-        return payload.decode('utf-8', 'strict')
+        return {self.field_names[0]: payload.decode('utf-8', 'strict')}
