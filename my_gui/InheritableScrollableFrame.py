67c57631428065798959641ae0a2e35178b73390
Izsó András <izso.andras@simonyi.bme.hu>
2022-01-20 20:38:40 +0100

    Scroll paramframe on mousewheel

--- my_gui/InheritableScrollableFrame.py
+++ my_gui/InheritableScrollableFrame.py
@@ -5,24 +5,35 @@ import tkinter
 class ScrollableFrame(tkinter.Frame):
     def __init__(self, parent, *args, **kwargs):
         self.parent = tkinter.Frame(parent)
-        canvas = tkinter.Canvas(self.parent)
-        super().__init__(canvas, *args, **kwargs)
-        scrollbar = tkinter.Scrollbar(self.parent, orient="vertical", command=canvas.yview)
+        self.canvas = tkinter.Canvas(self.parent)
+        super().__init__(self.canvas, *args, **kwargs)
+        self.scrollbar = tkinter.Scrollbar(self.parent, orient="vertical", command=self.canvas.yview)
 
         self.bind(
             "<Configure>",
-            lambda e: canvas.configure(
-                scrollregion=canvas.bbox("all"), width=self.winfo_width()
+            lambda e: self.canvas.configure(
+                scrollregion=self.canvas.bbox("all"), width=self.winfo_width()
             )
         )
 
-        canvas.create_window((0, 0), window=self, anchor="nw")
+        self.canvas.create_window((0, 0), window=self, anchor="nw")
 
-        canvas.configure(yscrollcommand=scrollbar.set)
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
 
-        scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
-        canvas.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=True)
+        self.scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+        self.canvas.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=True)
+        self.canvas.bind('<Enter>', self._bound_to_mousewheel)
+        self.canvas.bind('<Leave>', self._unbound_to_mousewheel)
         # self.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=True)
 
     def pack_parent(self, *args, **kwargs):
         self.parent.pack(*args, **kwargs)
+
+    def on_mouse_wheel(self, event):
+        self.canvas.yview_scroll(int(-1*(event.delta/120)),'units')
+
+    def _bound_to_mousewheel(self, event):
+        self.canvas.bind_all('<MouseWheel>', self.on_mouse_wheel)
+
+    def _unbound_to_mousewheel(self, event):
+        self.canvas.unbind_all('<MouseWheel>')
