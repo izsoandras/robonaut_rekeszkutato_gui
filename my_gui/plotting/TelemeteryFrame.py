9f6d4ee729a355801e278fd8ee36e596c3fc2fe5
Izsó András <izso.andras@simonyi.bme.hu>
2022-02-03 14:48:48 +0100

    Plot fill

--- my_gui/plotting/TelemeteryFrame.py
+++ my_gui/plotting/TelemeteryFrame.py
@@ -13,5 +13,5 @@ class TelemetryFrame(tkinter.Frame):
         self.plot_frame = my_gui.plotting.PlotsFrame.PlotsFrame(dataholders_by_name, plot_rec, self)
 
         self.fr_startstop.pack(side=tkinter.TOP)
-        self.plot_frame.pack(side=tkinter.TOP)
+        self.plot_frame.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=True)
 
